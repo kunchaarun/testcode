77356b3545498b738b306510ac2bb8b7633984a2
ramesh babu <ramesh babu@ramesh>
2018-10-13 12:32:51 +0530

    bye

--- MagentoTest/src/test/java/MagentoTest.java
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
@@ -15,7 +16,7 @@ public class MagentoTest
 		
 	
 		String url="http://www.magento.com";
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(url);
